67550be4d666449d88056ac97f2cb7881ab97bc4
Azan Bin Zahid <20100206@lums.edu.pk>
2019-12-29 00:35:04 +0500

    setup boilerplate for alarms state

---
+++ reducers/alarmReducer.js
@@ -0,0 +1,22 @@
+import {ADD_ALARM, DELETE_ALARM} from '../actions';
+
+const initialState = {
+  alarms: [],
+};
+
+const alarmReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ALARM:
+      console.log('time', action.payload);
+      return state;
+
+    case DELETE_ALARM:
+      console.log('time', action.payload);
+      return state;
+
+    default:
+      return state;
+  }
+};
+
+export default alarmReducer;
