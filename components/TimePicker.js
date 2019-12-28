d7ee0b402318e2c02ff170869987074951f4e50b
Azan Bin Zahid <20100206@lums.edu.pk>
2019-12-29 00:46:02 +0500

    import date-time-picker

---
+++ components/TimePicker.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {Button, View, TimePickerAndroid} from 'react-native';
+import {connect} from 'react-redux';
+
+import {addAlarm} from './actions/alarm';
+import DateTimePicker from 'react-native-modal-datetime-picker';
+
+class TimePicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDateTimePickerVisible: false,
+    };
+  }
+
+  showDateTimePicker = () => {
+    this.setState({isDateTimePickerVisible: true});
+  };
+
+  hideDateTimePicker = () => {
+    this.setState({isDateTimePickerVisible: false});
+  };
+
+  handleDatePicked = date => {
+    console.log('A date has been picked: ', date);
+    this.hideDateTimePicker();
+  };
+
+  render() {
+    return (
+      <>
+        <Button title="Show DatePicker" onPress={this.showDateTimePicker} />
+        <DateTimePicker
+          isVisible={this.state.isDateTimePickerVisible}
+          onConfirm={this.handleDatePicked}
+          onCancel={this.hideDateTimePicker}
+        />
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    alarms: state.alarms,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    add: time => {
+      dispatch(addAlarm(time));
+    },
+  };
+};
+
+// eslint-disable-next-line prettier/prettier
+export default connect(mapStateToProps, mapDispatchToProps)(TimePicker);
